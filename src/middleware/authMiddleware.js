0b0495039bd19e07e0a664e5f966345caaa9037f
VictorCDS-p <Victor90774p@gmail.com>
2026-03-08 05:57:00 -0300

    authcontext

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,30 +1,26 @@
 import { verifyToken } from "../utils/jwt.js";
+import { userRepository } from "../repositories/userRepository.js";
 
-export default function authMiddleware(req, res, next) {
-
+export default async function authMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).json({
-      message: "Token not provided"
-    });
+    return res.status(401).json({ message: "Token not provided" });
   }
 
   const token = authHeader.split(" ")[1];
 
   try {
-
     const decoded = verifyToken(token);
 
-    req.user = decoded;
+    const user = await userRepository.findById(decoded.id);
+    if (!user) {
+      return res.status(401).json({ message: "User no longer exists" });
+    }
 
+    req.user = decoded; 
     next();
-
   } catch (error) {
-
-    return res.status(401).json({
-      message: "Invalid token"
-    });
-
+    return res.status(401).json({ message: "Invalid token" });
   }
 }
